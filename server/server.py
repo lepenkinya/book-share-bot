20d3bd0bb139a6016ef4e617a7e4d6ebc5f0b1e9
Yaroslav Lepenkin <yaroslav.lepenkin@jetbrains.com>
2016-04-30 22:27:46 +0300

    inline mode test

--- server/server.py
+++ server/server.py
@@ -1,5 +1,4 @@
-import json
-import requests
+import json, requests, uuid
 
 from flask import Flask, jsonify, request
 
@@ -12,31 +11,57 @@ from telegram_api import telegram_method_url, telegram_get_file_url
 app = Flask(__name__)
 
 
+def handle_inline_query(query):
+    inline_query_id = query['id']
+    query = query['query']
+    results = [{'type': 'article', 'id': uuid.uuid4(), 'title': 'my_title',
+                'input_message_content': {'message_text': 'bot received query : ' + query}}]
+
+    return jsonify(method='answerInlineQuery', inline_query_id=inline_query_id, results=results)
+
+
+def handle_message(message):
+    if 'photo' in message:
+        return handle_sent_photo(message)
+
+    chat_id = message['chat']['id']
+    return jsonify(method='sendMessage', chat_id=chat_id, text='handling message no photo send')
+
+
 @app.route("/start", methods=['GET', 'POST'])
 def start():
     if request.method == 'POST':
         data = json.loads(request.data)
-        message = data['message']
-        max_photo_info = max(message['photo'], key=lambda x: x['width'])
-        file_id = max_photo_info['file_id']
 
-        url = get_file_url(file_id)
-        lines = get_lines_from_image(url)
+        if 'message' in data:
+            message = data['message']
+            return handle_message(message)
 
-        chat_id = message['chat']['id']
+        if 'inline_query' in data:
+            query = data['inline_query']
+            return handle_inline_query(query)
 
-        isbn = get_ISBN(lines)
-        if not len(isbn):
-            return jsonify(method='sendMessage', chat_id=chat_id, text='ISBN is not detected')
-
-        books = find_book_by_isbn(isbn)
-
-        if len(books):
-            return jsonify(method='sendMessage', chat_id=chat_id, text=books[0].to_string())
-
-        return jsonify(method='sendMessage', chat_id=chat_id, text='ISBN: ' + isbn)
     else:
         return jsonify(method='method', param='param')
+
+
+def handle_sent_photo(message):
+    max_photo_info = max(message['photo'], key=lambda x: x['width'])
+    file_id = max_photo_info['file_id']
+    url = get_file_url(file_id)
+    lines = get_lines_from_image(url)
+    chat_id = message['chat']['id']
+    isbn = get_ISBN(lines)
+
+    if not len(isbn):
+        return jsonify(method='sendMessage', chat_id=chat_id, text='ISBN is not detected')
+
+    books = find_book_by_isbn(isbn)
+
+    if len(books):
+        return jsonify(method='sendMessage', chat_id=chat_id, text=books[0].to_string())
+
+    return jsonify(method='sendMessage', chat_id=chat_id, text='ISBN: ' + isbn)
 
 
 @app.route("/")
